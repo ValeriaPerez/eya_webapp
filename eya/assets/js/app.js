993f4428491dcc7e4d4bd01fa61b0ee9091f8181
Valeria Perez <valeria@kamikazelab.com>
2018-10-30 21:43:51 -0600

    Ajuste de archivos js y scss general

--- eya/assets/js/app.js
+++ eya/assets/js/app.js
@@ -1,4 +1,7 @@
 import base from './base/base';
+import header from './base/header';
+import menu from './base/menu';
+
 import popupDetail from './products/popup_detail';
 import productCarousel from './owl-carousel/products-carousel';
 
@@ -7,6 +10,9 @@ import toggle from './users/toggle';
 import owl from './plugins/owl.carousel.min.js';
 
 base();
+header();
+menu();
+
 productCarousel();
 popupDetail();
 
